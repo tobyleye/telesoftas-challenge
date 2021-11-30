0603f10921517e7d1c843a0c39b9aabe47ba7a19
Tobi Oyeleye <krisella74@gmail.com>
2021-11-30 13:13:58 +0100

    dockerize our app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import "./styles.css";
 
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import { User } from "./views/User";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={User}></Route>
-        <Redirect from="*" to="/" />
+        <Route path={["/", "/user/:id"]} exact component={User}></Route>
+        <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return (
+    <div
+      style={{
+        background: "lightblue",
+        padding: 20,
+        textAlign: "center",
+      }}
+    >
+      <p>looks like you lost your way. Dont worry we will help you find it.</p>
+      <Link to="/">I'm lost</Link>
+    </div>
   );
 }
